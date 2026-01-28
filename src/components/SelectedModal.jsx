f410e804e8b4498d0e213629aef154d7046dae62
janvi <janviyadav120505@gmail.com>
2026-01-28 18:38:34 +0530

    feat: add react-icons dependency and update SelectedModal component for improved UI

--- src/components/SelectedModal.jsx
+++ src/components/SelectedModal.jsx
@@ -1,16 +1,22 @@
+import { IoClose } from "react-icons/io5";
+
 const SelectedModal = ({isOpen, onClose, shade}) => {
     if(!isOpen) return null;
     
     return(
-        <div className="fixed inset-0 bg-black/80 flex items-center justify-center z-50">
-            <div className="bg-white p-8 rounded-2xl">
-                <button onClick={onClose} className="mb-4 px-4 py-2 bg-red-500 text-white rounded">
-                    Close
-                </button>
-                <h1 className="text-2xl">Modal is working!</h1>
-                <p>{shade.colorname}</p>
+        <div className="fixed inset-0 bg-black/80 grid grid-cols-2 items-center  z-50">
+            <div className="ml-[20%]">
+                <img className="h-110  justify-center flex" src={shade.image} alt="" />
+                <IoClose className="text-white text-4xl" />
+
+
+                
+                
+            </div>
+            <div className="text-white">
+                dsiweifeh
             </div>
         </div>
     )
 }
-export default SelectedModal;
+export default SelectedModal; 
