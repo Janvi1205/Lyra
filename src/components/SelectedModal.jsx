1cf83c6c2494de4c2b80508b21070a4824ca6139
janvi <janviyadav120505@gmail.com>
2026-01-28 22:13:52 +0530

    fix: adjust margin for color name and button in SelectedModal for better alignment

--- src/components/SelectedModal.jsx
+++ src/components/SelectedModal.jsx
@@ -14,14 +14,14 @@ const SelectedModal = ({ isOpen, onClose, shade }) => {
 
             </div>
             <div className="text-white">
-                <div><h1 className="text-5xl font-serif -mt-6  ml-10">{shade.colorname}</h1></div>
+                <div><h1 className="text-5xl font-serif mt-5  ml-10">{shade.colorname}</h1></div>
                 <div> <p className="text-red-600 text-[25px] font-bold ml-10 mt-1 ">₹{shade.price}</p></div>
                 <div>  <p className="w-100 px-3  ml-8 mt-5 text-gray-300">{shade.description}</p></div>
                 <div className="flex mt-8 ml-11 gap-3">
                     <p className="text-[20px] " >Finish :</p>
                     <button className="border-red-700 rounded-xl border-2 px-3 mt-1">{shade.category}</button>
                 </div>
-                <div className="mt-10 ml-10">
+                <div className="mt-16 ml-10">
                     <button className="bg-white text-black py-3 px-32 rounded-xl flex cursor-pointer ">Add to cart
                         <p>  <HugeiconsIcon
                             className="ml-2  cursor-pointer"
