c122a8b616bdf46bb7254dc787b92f40397fa956
janvi <janviyadav120505@gmail.com>
2026-02-02 00:26:30 +0530

    feat: add navigation link to Shades in Hero component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,6 @@
 import { useLayoutEffect } from "react";
 import gsap from "gsap";
-
+import {Link} from "react-router-dom"
 const Hero = () => {
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
@@ -46,18 +46,18 @@ const Hero = () => {
 
   return (
     <div className="relative h-screen bg-black overflow-hidden">
-      
-      <div 
-        id="hero-image" 
-        className="absolute inset-0 hero-bg bg-cover bg-center ml-0 md:ml-150" 
+
+      <div
+        id="hero-image"
+        className="absolute inset-0 hero-bg bg-cover bg-center ml-0 md:ml-150"
       />
-      
-     
+
+
       <div className="absolute inset-0 bg-gradient-to-r from-black via-black/70 md:via-black/40 to-transparent" />
-      
-      
+
+
       <div className="flex flex-col items-start justify-center h-full">
-       
+
         <div id="hero-text" className="relative lg:-mt-50 sm:-mt-50 -mt-10  z-10 py-8 px-6 md:py-20 lg:py-30 md:px-12 lg:px-23">
           <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-white font-light leading-tight tracking-wide">
             Elegance In
@@ -71,14 +71,16 @@ const Hero = () => {
           </p>
         </div>
 
-       
+
         <div className="relative z-10 px-6 md:px-0 md:-mt-16 lg:-mt-23 md:ml-12 lg:ml-22">
-          <button 
-            id="hero-btn" 
-            className="px-6 md:px-8 py-2.5 md:py-3 bg-[#7b001c] text-white text-sm md:text-base rounded-xl hover:bg-[#a30028] transition cursor-pointer duration-300 shadow-lg"
-          >
-            Explore Shades
-          </button>
+          <Link to="/shades" >
+            <button
+              id="hero-btn"
+              className="px-6 md:px-8 py-2.5 md:py-3 bg-[#7b001c] text-white text-sm md:text-base rounded-xl hover:bg-[#a30028] transition cursor-pointer duration-300 shadow-lg"
+            >
+              Explore Shades
+            </button>
+          </Link>
         </div>
       </div>
     </div>
